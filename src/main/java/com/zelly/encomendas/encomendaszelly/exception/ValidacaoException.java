caa94ecab3c1f15a3d5d7ea86f931ee232f05d1e
Rafael de Jesus <rafael_rodrigues1993@yahoo.com.br>
2023-11-05 11:12:47 -0300

    Adicionando mensagens de validacao e tratamento de erros

---
+++ src/main/java/com/zelly/encomendas/encomendaszelly/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package com.zelly.encomendas.encomendaszelly.exception;
+
+public class ValidacaoException extends RuntimeException{
+    public ValidacaoException(String mensagem){
+        super(mensagem);
+    }
+}
